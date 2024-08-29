b2d6de8e75f38c13811b0f6a86ba5cc3b07389b1
jasper <whiteana001@warren.k12.in.us>
2024-08-29 13:55:41 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getDaysInMonth(1, 2020));
+        System.out.println(getDaysInMonth(2, 2020));
+        System.out.println(getDaysInMonth(2, 2018));
+        System.out.println(getDaysInMonth(-1, 2020));
+        System.out.println(getDaysInMonth(2, -2020));
+    }
+
+    private static boolean isLeapYear(int year) {
+        return div4Check(year);
+    }
+
+    private static boolean div4Check(int year) {
+        if (year % 4 == 0) {
+            return div100Check(year);
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean div100Check(int year) {
+        if (year % 100 == 0) {
+            return div400Check(year);
+        } else {
+            return true;
+        }
+    }
+
+    private static boolean div400Check(int year) {
+        return year % 400 == 0;
+    }
+
+    private static int getDaysInMonth(int month, int year) {
+        if (year < 1 || year > 9999) {
+            return -1;
+        }
+        return switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 2 -> {
+                if (isLeapYear(year)) {
+                    yield 29;
+                }
+                yield 28;
+            }
+            case 4, 6, 9, 11 -> 30;
+            default -> -1;
+
+        } ;
+    }
 }
+
